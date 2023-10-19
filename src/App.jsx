34f8240321f4a7920f94198b3408f4d87cec057f
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-19 20:10:45 +0330

    Finish :: apply global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 32rem;
+  width: 500px;
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div></div>
+    </>
+  );
 }
 
 export default App;
