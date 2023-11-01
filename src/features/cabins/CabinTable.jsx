c26d345139f1ea1a92e034f9424da88d0fcf0863
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-01 20:00:35 +0330

    Completed :: creating custom table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,21 +1,10 @@
 // v2
-import { useQuery } from "@tanstack/react-query";
+import { useCabins } from "./useCabins";
+
 import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
 import Spinner from "./../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import { useCabins } from "./useCabins";
-// Right now this is not really reusable... But we will want to use a similar table for guests as well, but with different columns. ALSO, right now we are defining these columns in BOTH the TableHeader and the CabinRow, which is not good at all. Instead, it would be much better to simply pass the columns into the Table, and the table would give access to the columns to both the header and row. So how can we do that? Well we can again use a compound component! We don't HAVE to do it like this, there's a million ways to implement a table, also without CSS Grid, but this is what I chose
-
-// v1
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+import Table from "./../../ui/Table";
 
 const TableHeader = styled.header`
   display: grid;
@@ -40,19 +29,20 @@ function CabinTable() {
   }
 
   return (
-    <Table role="table">
-      <TableHeader role="tow">
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
+      </Table.Header>
 
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
     </Table>
   );
 }
