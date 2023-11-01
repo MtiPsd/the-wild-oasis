8dadb59eddd7565a7c1d85ad83b83d912db1bf19
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-01 18:16:00 +0330

    Finished :: working on modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,23 +4,17 @@ import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
 
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      {/* <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window> */}
-    </Modal>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
